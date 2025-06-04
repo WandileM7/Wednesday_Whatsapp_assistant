faab2594a63e694483446146420d718159edce4e
WandileM7 <wandilemawela4@gmail.com>
2025-06-04 23:01:56 +0200

    registering authorize

--- handlers/google_auth.py
+++ handlers/google_auth.py
@@ -12,7 +12,11 @@ SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/gmail.send",
     "https://www.googleapis.com/auth/calendar.events",
-    "https://www.googleapis.com/auth/calendar.readonly"
+    "https://www.googleapis.com/auth/calendar.readonly",
+    "https://www.googleapis.com/auth/contacts.readonly",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "openid"
 ]
 
 # Get path to the pickle file where we store credentials
